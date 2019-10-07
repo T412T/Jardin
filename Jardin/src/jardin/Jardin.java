8a7a849055d57e226e23f26fc1704359572db9f1
Jean-maxime Alexandre <56034778+T412T@users.noreply.github.com>
2019-10-07 17:33:16 +0200

    Add files via upload

--- Jardin/src/jardin/Jardin.java
+++ Jardin/src/jardin/Jardin.java
@@ -20,6 +20,36 @@ public class Jardin {
 
 		for (String i : panier.keySet()) {
 			System.out.println(" key: " + i + " value: " + panier.get(i));
+		
+		Jardin jardin = new Jardin(10, 10);
+		
+		public String tostring() {
+		
+		string text ="Voici notre jardin"
+				
+		for(int i = 0;  i <longeur ; i++) {
+			for(int j = 0; j<largeur; j++) {
+				Emplacement e = emplacement [i][j];
+				if(e==null) {
+					texte += "o";
+					j else {
+						text +="Somthing";
+					}
+				}
+				texte += "/n";
+			}
+			text += "Et vottre panier contient : /n";
+					ajouterPanier("Tomate",5);
+					ajouterPanier("Carotte",5);
+					jouterPanier("Ail",10);
+					jouterPanier("Betrave",5);
+					
+					text += println	;
+			
+			return text
 		}
 	}
+		public void ajouterPanier(string s,int i) {
+			this.panier.compute(s , i)
+		}
 }
--- Jardin/src/jardin/Terre.java
+++ Jardin/src/jardin/Terre.java
@@ -3,7 +3,7 @@ public class Terre {
 
 	public static void main(String[] args) {
 		
-		Jardin jardin = new Jardin(10, 10);
+		
 
 	}
 
